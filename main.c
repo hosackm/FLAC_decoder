4595103c5b37cb708e059238da5fe41c02545fd7
Matt Hosack <hosack.matt@gmail.com>
2014-11-29 21:36:32 -0800

    Added comments and removed 32 bit mode since it was really just a shifted 16 bit version

--- main.c
+++ main.c
@@ -10,26 +10,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #include "FLAC/stream_decoder.h"
 #include "sndfile.h"
-
-//#define BIT32
 
 typedef struct
 {
     const char  *filename;
     SNDFILE     *sf;
-}my_client_data;
+}sndfile_data;
 
-FLAC__StreamDecoderWriteStatus write_callback(  const FLAC__StreamDecoder *dec
-                                                ,const FLAC__Frame *frame
-                                                ,const FLAC__int32 * const buffer[]
-                                                ,void *client_data);
+FLAC__StreamDecoderWriteStatus
+write_callback(  const FLAC__StreamDecoder *dec
+                ,const FLAC__Frame *frame
+                ,const FLAC__int32 * const buffer[]
+                ,void *client_data
+               );
 
 void error_callback(const FLAC__StreamDecoder *dec
                     ,FLAC__StreamDecoderErrorStatus status
-                    ,void *client_data);
+                    ,void *client_data
+                    );
 
 int main(int argc, const char * argv[])
 {
@@ -37,7 +37,10 @@ int main(int argc, const char * argv[])
     SF_INFO             info;
     FLAC__bool          ret;
     FLAC__StreamDecoder *decoder;
-    my_client_data      data;
+    sndfile_data        data;
+    const char *in_file = argv[1];
+    const char *out_file = argv[2];
+
     
     if (argc < 3)
     {
@@ -45,16 +48,14 @@ int main(int argc, const char * argv[])
         return -1;
     }
     
-    /*  We want to output a Stereo WAV File
-     *  32 bit depth
-     *  44.1 kHz sample rate */
+    /*  We want to output a Stereo WAV file
+     *  16 bit resolution
+     *  44.1 kHz sample rate 
+     */
     info.channels = 2;
     info.samplerate = 44100;
-#ifdef BIT32
-    info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_32;
-#else
     info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-#endif
+
     /* Verify we have a valid format */
     if(sf_format_check(&info) < 0)
     {
@@ -63,45 +64,49 @@ int main(int argc, const char * argv[])
     }
     
     /* Open our output file and check that it worked */
-    sf = sf_open(argv[2], SFM_WRITE, &info);
+    sf = sf_open(out_file, SFM_WRITE, &info);
 
     if (sf_error(sf) != SF_ERR_NO_ERROR) {
-        printf("Could not open file: %s. error %d\n", argv[2], sf_error(sf));
+        printf("Could not open file: %s. error %d\n", out_file, sf_error(sf));
         printf("%s\n", sf_strerror(sf));
         return -1;
     }
     
     /* Point our data to the SNDFILE */
     data.sf         = sf;
-    data.filename   = argv[1];
+    data.filename   = in_file;
     
     decoder = FLAC__stream_decoder_new();
     
+    /* We won't be handling any metadata or md5 checking for now */
     FLAC__stream_decoder_set_metadata_ignore_all(decoder);
-    
     FLAC__stream_decoder_set_md5_checking(decoder, false);
     
+    /* Initialize the decoder and point to our callbacks */
     ret = FLAC__stream_decoder_init_file(decoder
-                                         ,argv[1]
+                                         ,in_file
                                          ,write_callback
                                          ,NULL
                                          ,error_callback
-                                         ,&data);
+                                         ,&data
+                                         );
     
     if (ret != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
         printf("Error initializing FLAC Stream Decoder\n");
         return -1;
     }
     
-    sf_write_sync(sf);
-    
+    /* Begin the decoding process */
     ret = FLAC__stream_decoder_process_until_end_of_stream(decoder);
     
+    /* Check the process call's return value */
     if (ret == false)
     {
         printf("Fatal error during decoding process!\n");
     }
     
+    
+    /* Clean up */
     ret = FLAC__stream_decoder_finish(decoder);
     
     if (ret == false)
@@ -116,55 +121,44 @@ int main(int argc, const char * argv[])
     return 0;
 }
 
-FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *dec
-                                              ,const FLAC__Frame *frame
-                                              ,const FLAC__int32 * const buffer[]
-                                              ,void *client_data)
+FLAC__StreamDecoderWriteStatus
+write_callback( const FLAC__StreamDecoder *dec
+               ,const FLAC__Frame *frame
+               ,const FLAC__int32 * const buffer[]
+               ,void *client_data)
 {
-#ifdef BIT32
-    FLAC__int32         *p_samples;
-#else
-    FLAC__int16         *p_samples;
-#endif
-    unsigned int        sample_idx;
-    unsigned int        channel_idx;
-    my_client_data      *data       = (my_client_data*)client_data;
-    const unsigned int  channels    = frame->header.channels;
-    const unsigned int  blocksize   = frame->header.blocksize;
+    FLAC__int16         *p_samples;         /* Buffer for holding interleaved samples */
+    unsigned int        sample_idx;         /* Index of the current sample we are on  */
+    unsigned int        channel_idx;        /* Index of the current channel we are on */
+    sndfile_data      *data       = (sndfile_data*)client_data; /* Cast data provided by libFLAC */
+    const unsigned int  channels    = frame->header.channels;       /* Number of channels decoded by libFLAC */
+    const unsigned int  blocksize   = frame->header.blocksize;      /*  Number of samples decoded by libFLAC.
+                                                                    *   Each sample contains a Left and Right channel
+                                                                    *   because we are only handling stereo
+                                                                    */
 
-#ifdef BIT32
-    p_samples = (FLAC__int32*) malloc(sizeof(FLAC__int32) * channels * blocksize );
-    memset(p_samples, 0, sizeof(FLAC__int32) * channels * blocksize );
-#else
-    p_samples = (FLAC__int16*) malloc(sizeof(FLAC__int16) * channels * blocksize );
+    
+    /* Allocate memory for our interleaved buffer we will be populating */
+    p_samples = (FLAC__int16*) malloc(sizeof(FLAC__int16) * channels * blocksize);
+    /* Always a good idea to zero-out the whole thing before writing to it */
     memset(p_samples, 0, sizeof(FLAC__int16) * channels * blocksize );
-#endif
+    
     // Copy from the FLAC buffers into an interleaved array of samples
-#ifdef BIT32
-    for ( sample_idx = 0;  sample_idx < blocksize ; ++sample_idx )
+    for (sample_idx = 0;  sample_idx < blocksize ; ++sample_idx)
     {
-        for(channel_idx = 0; channel_idx < channels; ++channel_idx )
-        {
-            *p_samples++ = buffer[channel_idx][sample_idx] << 16;
-        }
-    }
-    p_samples -= blocksize  * channels;
-#else
-    for ( sample_idx = 0;  sample_idx < blocksize ; ++sample_idx )
-    {
-        for(channel_idx = 0; channel_idx < channels; ++channel_idx )
+        for(channel_idx = 0; channel_idx < channels; ++channel_idx)
         {
             *p_samples++ = /*(FLAC__int16)*/ buffer[channel_idx][sample_idx] ;
         }
     }
+    
+    /* Decrement our p_samples pointer to it's first sample */
     p_samples -= blocksize  * channels;
-#endif
-    // Write from interleaved buffer
-#ifdef BIT32
-    sf_write_int(data->sf, p_samples, blocksize  * channels);
-#else
+    
+    /* Write to wavfile from interleaved buffer */
     sf_write_short(data->sf, p_samples, blocksize * channels);
-#endif
+    
+    /* Free allocated memory to avoid a memory leak */
     free(p_samples);
     
     return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
@@ -174,7 +168,7 @@ void error_callback(const FLAC__StreamDecoder *dec
                     ,FLAC__StreamDecoderErrorStatus status
                     ,void *client_data)
 {
-    my_client_data *d = (my_client_data *)client_data;
+    sndfile_data *d = (sndfile_data *)client_data;
     printf("'Error Callback' called while decoding\
            %s but not handled very well...\n" ,d->filename);
 }
