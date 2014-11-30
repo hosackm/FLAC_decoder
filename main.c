24f6cfc478c5bde38f65443770a6b73e08fd666c
Matt Hosack <hosack.matt@gmail.com>
2014-11-29 21:14:31 -0800

    Added ifdefs for switching between 32 and 16 bit

--- main.c
+++ main.c
@@ -14,14 +14,14 @@
 #include "FLAC/stream_decoder.h"
 #include "sndfile.h"
 
+//#define BIT32
+
 typedef struct
 {
-    SNDFILE *f;
+    const char  *filename;
+    SNDFILE     *sf;
 }my_client_data;
 
-/****************************************************************************
-************************* CALLBACK PROTOS ***********************************
-****************************************************************************/
 FLAC__StreamDecoderWriteStatus write_callback(  const FLAC__StreamDecoder *dec
                                                 ,const FLAC__Frame *frame
                                                 ,const FLAC__int32 * const buffer[]
@@ -30,17 +30,14 @@ FLAC__StreamDecoderWriteStatus write_callback(  const FLAC__StreamDecoder *dec
 void error_callback(const FLAC__StreamDecoder *dec
                     ,FLAC__StreamDecoderErrorStatus status
                     ,void *client_data);
-/***************************************************************************/
 
 int main(int argc, const char * argv[])
 {
-    FLAC__bool ret;
+    SNDFILE             *sf;
+    SF_INFO             info;
+    FLAC__bool          ret;
     FLAC__StreamDecoder *decoder;
-    
-    SNDFILE *f;
-    SF_INFO info;
-    
-    my_client_data data;
+    my_client_data      data;
     
     if (argc < 3)
     {
@@ -48,25 +45,35 @@ int main(int argc, const char * argv[])
         return -1;
     }
     
+    /*  We want to output a Stereo WAV File
+     *  32 bit depth
+     *  44.1 kHz sample rate */
     info.channels = 2;
     info.samplerate = 44100;
+#ifdef BIT32
     info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_32;
-    
+#else
+    info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+#endif
+    /* Verify we have a valid format */
     if(sf_format_check(&info) < 0)
     {
         printf("File format not correct\n");
         return -1;
     }
     
-    f = sf_open(argv[2], SFM_WRITE, &info);
-    
-    if (sf_error(f) != SF_ERR_NO_ERROR) {
-        printf("Could not open file: %s. error %d\n", argv[2], sf_error(f));
-        printf("%s\n", sf_strerror(f));
+    /* Open our output file and check that it worked */
+    sf = sf_open(argv[2], SFM_WRITE, &info);
+
+    if (sf_error(sf) != SF_ERR_NO_ERROR) {
+        printf("Could not open file: %s. error %d\n", argv[2], sf_error(sf));
+        printf("%s\n", sf_strerror(sf));
         return -1;
     }
     
-    data.f = f;
+    /* Point our data to the SNDFILE */
+    data.sf         = sf;
+    data.filename   = argv[1];
     
     decoder = FLAC__stream_decoder_new();
     
@@ -86,7 +93,7 @@ int main(int argc, const char * argv[])
         return -1;
     }
     
-    sf_write_sync(f);
+    sf_write_sync(sf);
     
     ret = FLAC__stream_decoder_process_until_end_of_stream(decoder);
     
@@ -104,7 +111,7 @@ int main(int argc, const char * argv[])
     
     FLAC__stream_decoder_delete(decoder);
     
-    sf_close(f);
+    sf_close(sf);
     
     return 0;
 }
@@ -114,25 +121,50 @@ FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *dec
                                               ,const FLAC__int32 * const buffer[]
                                               ,void *client_data)
 {
-    FLAC__int32 *p_samples, *p_read;
-    unsigned int samp, ch;
-    const unsigned int length = frame->header.blocksize;
-    const unsigned int channels = frame->header.channels;
-    my_client_data *data = (my_client_data*)client_data;
+#ifdef BIT32
+    FLAC__int32         *p_samples;
+#else
+    FLAC__int16         *p_samples;
+#endif
+    unsigned int        sample_idx;
+    unsigned int        channel_idx;
+    my_client_data      *data       = (my_client_data*)client_data;
+    const unsigned int  channels    = frame->header.channels;
+    const unsigned int  blocksize   = frame->header.blocksize;
 
-    p_samples = (FLAC__int32*) malloc(sizeof(FLAC__int32) * channels *length);
-    memset(p_samples, 0.0, sizeof(FLAC__int32) * channels * length);
-    
-    p_read = p_samples;
-    for (samp = 0; samp < length; ++samp) {
-        for(ch = 0; ch < channels; ++ch)
+#ifdef BIT32
+    p_samples = (FLAC__int32*) malloc(sizeof(FLAC__int32) * channels * blocksize );
+    memset(p_samples, 0, sizeof(FLAC__int32) * channels * blocksize );
+#else
+    p_samples = (FLAC__int16*) malloc(sizeof(FLAC__int16) * channels * blocksize );
+    memset(p_samples, 0, sizeof(FLAC__int16) * channels * blocksize );
+#endif
+    // Copy from the FLAC buffers into an interleaved array of samples
+#ifdef BIT32
+    for ( sample_idx = 0;  sample_idx < blocksize ; ++sample_idx )
+    {
+        for(channel_idx = 0; channel_idx < channels; ++channel_idx )
         {
-            *p_read++ = (buffer[ch][samp] << 16);
+            *p_samples++ = buffer[channel_idx][sample_idx] << 16;
         }
     }
-    
+    p_samples -= blocksize  * channels;
+#else
+    for ( sample_idx = 0;  sample_idx < blocksize ; ++sample_idx )
+    {
+        for(channel_idx = 0; channel_idx < channels; ++channel_idx )
+        {
+            *p_samples++ = /*(FLAC__int16)*/ buffer[channel_idx][sample_idx] ;
+        }
+    }
+    p_samples -= blocksize  * channels;
+#endif
     // Write from interleaved buffer
-    sf_write_int(data->f, p_samples, length * channels);
+#ifdef BIT32
+    sf_write_int(data->sf, p_samples, blocksize  * channels);
+#else
+    sf_write_short(data->sf, p_samples, blocksize * channels);
+#endif
     free(p_samples);
     
     return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
@@ -142,5 +174,7 @@ void error_callback(const FLAC__StreamDecoder *dec
                     ,FLAC__StreamDecoderErrorStatus status
                     ,void *client_data)
 {
-    printf("Error Callback called but not handled very well...\n");
+    my_client_data *d = (my_client_data *)client_data;
+    printf("'Error Callback' called while decoding\
+           %s but not handled very well...\n" ,d->filename);
 }
